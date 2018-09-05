8d8f14483bbdd9d4d045cdd4514f3e261c027eca
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-09-05 14:45:58 -0600

    completes iteration 3

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,7 +1,40 @@
+require 'pry'
 class Dock
-    attr_reader :name, :max_rental_time
+    attr_reader :name, :max_rental_time, :rented_boats, :returned_boats
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @rented_boats = []
+    @returned_boats = []
   end
+
+  def rent(boat, renter)
+    @rented_boats << boat
+  end
+
+  def log_hour
+    @rented_boats.map do |boat|
+      boat.hours_rented +=1
+    end
+  end
+
+  def return(boat)
+    @rented_boats.each do |b|
+      if b == boat
+        @returned_boats << b
+        @rented_boats.delete(boat)
+      end
+    end
+
+  end
+
+  def revenue
+    revenue = 0
+    @returned_boats.each do |boat|
+    revenue += boat.price_per_hour * boat.hours_rented
+    end
+    revenue
+  end
+
+
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -12,11 +12,95 @@ class RenterTest < Minitest::Test
   end
 
   def test_dock_has_attributes
-    dock = Dock.new("The Rowing Dock", 3)   
+    dock = Dock.new("The Rowing Dock", 3)
 
     assert_equal "The Rowing Dock", dock.name
 
     assert_equal 3, dock.max_rental_time
   end
 
+
+  def test_dock_can_rent_a_boat_to_a_renter
+    dock = Dock.new("The Rowing Dock", 3)
+
+    kayak_1 = Boat.new(:kayak, 20)
+
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    dock.rent(kayak_1, patrick)
+
+    # dock.log_hour(kayak_1)
+
+    # dock.return(kayak_1)
+
+    assert_equal [kayak_1], dock.rented_boats
+  end
+
+  def test_can_log_hours_for_rented_boat
+    dock = Dock.new("The Rowing Dock", 3)
+
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    dock.rent(kayak_1, patrick)
+
+
+    dock.log_hour
+
+    dock.rent(kayak_2, patrick)
+
+    dock.log_hour
+
+    assert_equal 2, kayak_1.hours_rented
+
+    assert_equal 1, kayak_2.hours_rented
+  end
+
+  def test_boat_can_be_returned
+    dock = Dock.new("The Rowing Dock", 3)
+
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    dock.rent(kayak_1, patrick)
+
+
+    dock.log_hour
+
+    dock.rent(kayak_2, patrick)
+
+    dock.log_hour
+
+    dock.return(kayak_1)
+
+    assert_equal [kayak_2], dock.rented_boats
+
+    assert_equal [kayak_1], dock.returned_boats
+  end
+#
+  def test_can_calculate_total_revenue
+    dock = Dock.new("The Rowing Dock", 3)
+
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+
+    dock.rent(kayak_1, patrick)
+
+
+    dock.log_hour
+
+    dock.rent(kayak_2, patrick)
+
+    dock.log_hour
+
+    dock.return(kayak_1)
+
+    assert_equal 40, dock.revenue
+  end
 end
